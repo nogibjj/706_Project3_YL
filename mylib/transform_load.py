839587cdd9d137cba7ddfaf6cd48e7872e55bfab
Yue Li <michaelli0914@outlook.com>
2023-11-17 03:15:01 +0000

    change transform_load

---
+++ mylib/transform_load.py
@@ -0,0 +1,6 @@
+# from pyspark.sql import SparkSession
+# from pyspark.sql.functions 
+
+# def load(dataset1="dbfs:/FileStore/individual_project3/wc-20140609-140000.csv",
+#          dataset2="dbfs:/FileStore/individual_project3/wc-20140613-205820.csv"):
+    # spark = SparkSession.bu
